7a343f213db02f3f6e7abbd582028be545633501
emilti <emilti1986@gmail.com>
2015-09-26 04:17:52 +0300

    Added functionality for editing the notes - every edited note is saved as separate object.

--- controllers/notebook-controller.js
+++ controllers/notebook-controller.js
@@ -42,10 +42,31 @@ var notebookController = function() {
                                 $('.current .panel-body').html(sortedNotesFromServer[j].content);
                                 $('.current').append('<div class="operation-buttons row"/>')
                                 appendingButtons();
-
                                 $('.notes-container').children().removeClass('current');
                             }
                         }
+
+                        $('.btn-edit-note').on('click', function () {
+                            var $this = $(this);
+                            $this.parent().parent().parent().addClass('current');
+                            $('.btn-edit-note').prop("disabled", true);
+                            $('.current .btn-save-note').prop("disabled", false);
+
+                            // Getting the current data from the edited note
+                            var currentTitle = $('.current .panel-title').html();
+                            var currentContent = $('.current .panel-body').html();
+
+                            // removing the note elements and adding the elements for editing - input field and textarea
+                            $('.current .panel-heading').remove();
+                            $('.current .panel-body').remove();
+                            $('.current').prepend('<div class="adding-body"/>')
+                            $('.current .adding-body').append('<textarea style="resize:none" rows="4"/>');
+                            $('.current .adding-body textarea').addClass('note-content form-control').val(currentContent);
+                            $('.current').prepend('<div class="adding-heading"/>');
+                            $('.current .adding-heading').append('<input type="text" />');
+                            $('.current .adding-heading input').addClass('note-title form-control').val(currentTitle);
+                            saveNote();
+                        });
                     })
                 }
 
@@ -113,14 +134,13 @@ var notebookController = function() {
                      Parse.User.logOut();
                      localStorage.clear();
                      context.redirect('#/');
-                }
-             );
+                });
          })
 
         function appendingButtons() {
             $('.current .operation-buttons').append('<div class="col-md-offset-5 col-md-2"><button class="btn btn-md btn-success form-control btn-save-note" disabled>Save</div>')
                 .append('<div class="col-md-2"><button class="btn btn-md btn-primary form-control btn-edit-note">Edit</button></div>')
-                .append('<div class="col-md-2"><button class="btn btn-md btn-danger form-control btn-edit-note">Delete</button></div>')
+                .append('<div class="col-md-2"><button class="btn btn-md btn-danger form-control btn-delete-note">Delete</button></div>')
         }
      }
     return {
